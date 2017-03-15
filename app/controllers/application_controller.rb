468ff788fa3c9d01e7fb3824ec702c0872f98114
kapil kumar <kapilkumar660@gmail.com>
2017-03-15 08:22:55 +0530

    2 added link to most recent album in navigtion bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :get_recent_album
+  
+  def get_recent_album
+    @rec_album = Album.order("released_on").last
+  end
 end
